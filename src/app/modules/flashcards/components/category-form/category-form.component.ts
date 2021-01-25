5ae37e109b1fe9e315d804742fc1d7e8a3828720
kamatuszewski <s16704@pjwstk.edu.pl>
2021-01-25 19:04:45 +0100

    Dodanie polskich tekstów oraz wyświetlanie komunikatu w przypadku błędu zwróconego z serwera

--- src/app/modules/flashcards/components/category-form/category-form.component.ts
+++ src/app/modules/flashcards/components/category-form/category-form.component.ts
@@ -1,7 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Subject } from 'rxjs';
+import { TranslocoService } from '@ngneat/transloco';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { EAlert } from '../../../share/enums/alert.enum';
+import { IAlert } from '../../../share/interfaces/alert.interface';
 import { CategoryService } from '../../services/category.service';
 
 @Component({
@@ -10,10 +14,15 @@ import { CategoryService } from '../../services/category.service';
   styleUrls: ['./category-form.component.scss']
 })
 export class CategoryFormComponent implements OnInit, OnDestroy {
+  public alert$: BehaviorSubject<IAlert | void> = new BehaviorSubject<IAlert | null>(null);
   public formGroup: FormGroup;
+
   private onDestroy$ = new Subject<void>();
 
-  constructor(private formBuilder: FormBuilder, private categoryService: CategoryService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService,
+    private transloco: TranslocoService) {
     this.initFormGroup();
   }
 
@@ -21,15 +30,30 @@ export class CategoryFormComponent implements OnInit, OnDestroy {
     if (this.formGroup.valid) {
       this.categoryService.createCategory(this.formGroup.value)
         .pipe(takeUntil(this.onDestroy$))
-        .subscribe();
+        .subscribe(
+          () => console.log(),
+          (error: HttpErrorResponse) => {
+            const badRequest = 400;
+            if (error.status === badRequest) {
+              this.dispatchAlert(EAlert.ERROR, `ERRORS.${error.error.message}`);
+            }
+          }
+        );
     }
   }
 
-  public ngOnInit(): void {
+  public dispatchAlert(status: EAlert, text: string): void {
+    this.alert$.next({
+      text: this.transloco.translate(text),
+      type: status
+    })
   }
 
   public ngOnDestroy(): void {
     this.onDestroy$.next();
+  }
+
+  public ngOnInit(): void {
   }
 
   private initFormGroup(): void {
